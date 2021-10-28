48935ea5afb78c6fed25d1787c0eddd75434da78
s11152915 <70713796+s11152915@users.noreply.github.com>
2021-10-28 14:00:35 -0400

    Update variabless_practice.py

--- variables/variabless_practice.py
+++ variables/variabless_practice.py
@@ -1,1 +1,5 @@
 # variable Practice, Jermaine Jeffcoat, 10/28/21 1:46pm, version 0.0
+
+# Common Data Types and Variables in Python
+
+life_count = 3 # Interger value, all whole numbers, +/- and 0. 
